5bee5ca128b36ea5933c332cb9367259544a3682
francisco.mendes <francisco.mendes@ccc.ufcg.edu.br>
2025-11-30 17:07:23 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	sess, err := discordgo.New("Bot " + "MY TOKEN HERE")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+		if m.Content == "ping" {
+			s.ChannelMessageSend(m.ChannelID, "pong")
+		}
+	})
+
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = sess.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer sess.Close()
+
+	fmt.Println("BOT IS ONLINE!!!")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+}
